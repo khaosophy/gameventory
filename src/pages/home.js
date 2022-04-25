53818748183aaf7dde974a651f370e1136f86ed5
Casey James Perno <casey@khaosophy.com>
2022-04-25 15:35:35 -0400

    added logout button

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,22 @@
-import { Link } from "react-router-dom"
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom'
+import { auth, logout } from '../firebase';
 
 export default function Home() {
+  const [user, loading] = useAuthState(auth);
+
   return (
     <div className="container">
       <h1>Hello World!</h1>
       <p>Review <Link to="/games">your board game collection.</Link></p>
+      {user && !loading && (
+        <button
+          className="btn btn-danger"
+          onClick={logout}
+        >
+          Log out
+        </button>
+      )}
     </div>
   )
 }
