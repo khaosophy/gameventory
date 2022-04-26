06bad1ec50726f5a497e67251cdd6f6523d84658
Casey James Perno <casey@khaosophy.com>
2022-04-26 15:23:16 -0400

    convert game list to table

--- src/pages/games-list.js
+++ src/pages/games-list.js
@@ -13,11 +13,22 @@ export default function GamesList() {
     <PageTemplate>
       <div className="container">
         <h1>Board Games!</h1>
-        <ul>
-          {games.map(game => (
-            <li key={game.name}>{game.name}</li>
-          ))}
-        </ul>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Game Name</th>
+              <th>Number of Players</th>
+            </tr>
+          </thead>
+          <tbody>
+            {games.map(game => (
+              <tr key={game.name}>
+                <td>{game.name}</td>
+                <td>{game.minPlayers} - {game.maxPlayers}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </PageTemplate>
   );
