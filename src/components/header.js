5e26cc253ec9662f4f17cf26418cb41ce3303e96
Casey James Perno <casey@khaosophy.com>
2022-06-22 15:47:00 -0400

    add logo

--- src/components/header.js
+++ src/components/header.js
@@ -2,17 +2,16 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import { auth, logout } from '../firebase';
 
+import logo from '../assets/logo.png';
+
 export default function Header() {
   const [user, loading] = useAuthState(auth);
   return (
     <header className="border-bottom">
       <div className="container py-2 d-flex align-items-center justify-content-between">
-        <h1 className="logo">
-          {/* todo: logo */}
-          <Link to="/">
-            GameBook
-          </Link>
-        </h1>
+        <Link to="/">
+          <img src={logo} className="logo" alt="GameBook Logo" />
+        </Link>
         <nav></nav>
         <div>
           {user && !loading ? (
