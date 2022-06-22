363598c6ff890afaa8ed07ab22a5b0700693055a
Casey James Perno <casey@khaosophy.com>
2022-06-22 14:37:53 -0400

    update header styles

--- src/components/header.js
+++ src/components/header.js
@@ -5,27 +5,29 @@ import { auth, logout } from '../firebase';
 export default function Header() {
   const [user, loading] = useAuthState(auth);
   return (
-    <header className="container py-2 d-flex align-items-center justify-content-between">
-      <h1 className="logo">
-        {/* todo: logo */}
-        <Link to="/">
-          GameBook
-        </Link>
-      </h1>
-      <nav></nav>
-      <div>
-        {user && !loading ? (
-          <button
-            className="btn btn-danger"
-            onClick={logout}
-          >
-            Log out
-          </button>
-        ) : (
-          <Link to="/login" className="btn btn-primary">
-            Login
+    <header className="border-bottom">
+      <div className="container py-2 d-flex align-items-center justify-content-between">
+        <h1 className="logo">
+          {/* todo: logo */}
+          <Link to="/">
+            GameBook
           </Link>
-        )}
+        </h1>
+        <nav></nav>
+        <div>
+          {user && !loading ? (
+            <button
+              className="btn btn-danger"
+              onClick={logout}
+            >
+              Log out
+            </button>
+          ) : (
+            <Link to="/login" className="btn btn-primary">
+              Login
+            </Link>
+          )}
+        </div>
       </div>
     </header>
   )
--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -5,11 +5,8 @@ export default function PageTemplate({children}) {
   return (<>
     <Header />
     <main 
-      className="py-3"
-      style={{
-        boxShadow: 'inset 0 0.5em 1.5em rgb(0 0 0 / 10%), inset 0 0.125em 0.5em rgb(0 0 0 / 15%)',
-        flex: 1,
-      }}
+      className="py-3 bg-light"
+      style={{ flex: 1 }}
     >
       {children}
     </main>
