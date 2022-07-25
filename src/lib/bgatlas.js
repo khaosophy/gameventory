1cc9ab72b551d1658861aa554e267d1663860019
Casey James Perno <casey@khaosophy.com>
2022-07-25 15:14:12 -0400

    removed axios

--- src/lib/bgatlas.js
+++ src/lib/bgatlas.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
 
 export const apiBaseUrl = 'https://api.boardgameatlas.com/api/';
 export const clientId = process.env.REACT_APP_BGATLAS_API_CLIENT_ID;
 
 export const bgSearch = async (name) => {
   const searchUrl = `${apiBaseUrl}/search?client_id=${clientId}&name=${name}`;
-  const response = await axios.get(searchUrl);
-  return response.data.games;
+  const response = await fetch(searchUrl);
+  const data = await response.json();
+  return data.games;
 }
